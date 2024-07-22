c9ba320a41b4519f62c8b2f31b7f7daca4338d99
Michael Gnaho <gnahomichael@gmail.com>
2024-07-22 22:39:53 +0100

    uodate

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,20 @@
+export default function taskBlock(trueOrFalse) {
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    const task = true;
+    const task2 = false;
+    if (task && task2) {
+      return [task, task2];
+      alx -
+        program / alx -
+        frontend -
+        javascript / 0x00 -
+        ES6_basic / 2 -
+        arrow.js;
+    }
+  }
+
+  return [task, task2];
+}
